8b00a2259ecf43081d1436285c2d13fbb0c188cb
Vincent Schweiger <vincent.schweiger@xolley.de>
2024-08-21 15:12:32 +0200

    clang format, include assert.h

--- heap.h
+++ heap.h
@@ -1,26 +1,29 @@
 #pragma once
 
 #include <stddef.h>
+#include <assert.h>
 
-#define HEAP_ENTRY(type)                                                       \
-  struct {                                                                     \
-    struct type *child;                                                        \
-    struct type *prev;                                                         \
-    struct type *next;                                                         \
-  }
+#define HEAP_ENTRY(type)            \
+	struct {                    \
+		struct type *child; \
+		struct type *prev;  \
+		struct type *next;  \
+	}
 
-#define HEAP_INIT(head)                                                        \
-  do {                                                                         \
-    (head)->root = NULL;                                                       \
-  } while (0)
+#define HEAP_INIT(head)              \
+	do {                         \
+		(head)->root = NULL; \
+	} while (0)
 
-#define HEAP_INITIALIZER(root)                                                 \
-  { NULL }
+#define HEAP_INITIALIZER(root) \
+	{                      \
+		NULL           \
+	}
 
-#define HEAP_HEAD(name, type)                                                  \
-  struct name {                                                                \
-    struct type *root;                                                         \
-  }
+#define HEAP_HEAD(name, type)      \
+	struct name {              \
+		struct type *root; \
+	}
 
 #define HEAP_CHILD(elm, field) (elm)->field.child
 #define HEAP_PREV(elm, field) (elm)->field.prev
@@ -33,112 +36,120 @@
 #define HEAP_INSERT(name, head, x) (name##_HEAP_INSERT((head), (x)))
 #define HEAP_REMOVE(name, head, x) (name##_HEAP_REMOVE((head), (x)))
 
-#define HEAP_PROTOTYPE(name, type)                                             \
-  void name##_HEAP_INSERT(struct name *head, struct type *elm);                \
-  struct type *name##_HEAP_DELETE_MIN(struct name *head);                      \
-  void name##_HEAP_REMOVE(struct name *head, struct type *elm);
+#define HEAP_PROTOTYPE(name, type)                                    \
+	void name##_HEAP_INSERT(struct name *head, struct type *elm); \
+	struct type *name##_HEAP_DELETE_MIN(struct name *head);       \
+	void name##_HEAP_REMOVE(struct name *head, struct type *elm);
 
-#define HEAP_IMPL(name, type, field, cmp)                                      \
-  static struct type *name##_HEAP_MELD(struct type *a, struct type *b) {       \
-    assert(HEAP_NEXT(a, field) == NULL);                                       \
-    if ((cmp)(a, b)) {                                                         \
-      if (HEAP_CHILD(a, field) != NULL) {                                      \
-        HEAP_PREV(HEAP_CHILD(a, field), field) = b;                            \
-      }                                                                        \
-      if (HEAP_NEXT(b, field) != NULL) {                                       \
-        HEAP_PREV(HEAP_NEXT(b, field), field) = a;                             \
-      }                                                                        \
-      HEAP_NEXT(a, field) = HEAP_NEXT(b, field);                               \
-      HEAP_NEXT(b, field) = HEAP_CHILD(a, field);                              \
-      HEAP_CHILD(a, field) = b;                                                \
-      HEAP_PREV(b, field) = a;                                                 \
-      return a;                                                                \
-    }                                                                          \
-    if (HEAP_CHILD(b, field) != NULL) {                                        \
-      HEAP_PREV(HEAP_CHILD(b, field), field) = a;                              \
-    }                                                                          \
-    if (HEAP_PREV(a, field) != NULL &&                                         \
-        HEAP_CHILD(HEAP_PREV(a, field), field) != a) {                         \
-      HEAP_NEXT(HEAP_PREV(a, field), field) = b;                               \
-    }                                                                          \
-    HEAP_PREV(b, field) = HEAP_PREV(a, field);                                 \
-    HEAP_PREV(a, field) = b;                                                   \
-    HEAP_NEXT(a, field) = HEAP_CHILD(b, field);                                \
-    HEAP_CHILD(b, field) = a;                                                  \
-    return b;                                                                  \
-  }                                                                            \
-  static struct type *name##_HEAP_COMBINE_SIBLINGS(struct type *left) {        \
-    HEAP_PREV(left, field) = NULL;                                             \
-    struct type *root = NULL;                                                  \
-    struct type *a = left;                                                     \
-    while (1) {                                                                \
-      if (HEAP_NEXT(a, field) == NULL) {                                       \
-        root = a;                                                              \
-        break;                                                                 \
-      }                                                                        \
-      struct type *b = HEAP_NEXT(a, field);                                    \
-      HEAP_NEXT(a, field) = NULL;                                              \
-      b = name##_HEAP_MELD(a, b);                                              \
-      if (HEAP_NEXT(b, field) == NULL) {                                       \
-        root = b;                                                              \
-        break;                                                                 \
-      }                                                                        \
-      a = HEAP_NEXT(b, field);                                                 \
-    }                                                                          \
-    while (1) {                                                                \
-      if (HEAP_PREV(root, field) == NULL) {                                    \
-        return root;                                                           \
-      }                                                                        \
-      struct type *b = HEAP_PREV(root, field);                                 \
-      HEAP_NEXT(b, field) = NULL;                                              \
-      root = name##_HEAP_MELD(b, root);                                        \
-    }                                                                          \
-  }                                                                            \
-  struct type *name##_HEAP_DELETE_MIN(struct name *head) {                     \
-    if (HEAP_ROOT(head) == NULL)                                               \
-      return NULL;                                                             \
-    struct type *root = HEAP_ROOT(head);                                       \
-    if (HEAP_CHILD(root, field) != NULL) {                                     \
-      struct type *root_child = HEAP_CHILD(root, field);                       \
-      HEAP_ROOT(head) = name##_HEAP_COMBINE_SIBLINGS(root_child);              \
-    } else {                                                                   \
-      HEAP_ROOT(head) = NULL;                                                  \
-    }                                                                          \
-    HEAP_CHILD(root, field) = NULL;                                            \
-    HEAP_PREV(root, field) = NULL;                                             \
-    HEAP_NEXT(root, field) = NULL;                                             \
-    return root;                                                               \
-  }                                                                            \
-  void name##_HEAP_REMOVE(struct name *head, struct type *elm) {               \
-    if (HEAP_PREV(elm, field) == NULL) {                                       \
-      assert(HEAP_ROOT(head) == elm);                                          \
-      name##_HEAP_DELETE_MIN(head);                                            \
-      return;                                                                  \
-    }                                                                          \
-    struct type *prev = HEAP_PREV(elm, field);                                 \
-    if (HEAP_NEXT(elm, field) != NULL) {                                       \
-      struct type *elm_next = HEAP_NEXT(elm, field);                           \
-      HEAP_PREV(elm_next, field) = prev;                                       \
-    }                                                                          \
-    if (HEAP_CHILD(prev, field) == elm) {                                      \
-      HEAP_CHILD(prev, field) = HEAP_NEXT(elm, field);                         \
-    } else {                                                                   \
-      HEAP_NEXT(prev, field) = HEAP_NEXT(elm, field);                          \
-    }                                                                          \
-    HEAP_PREV(elm, field) = NULL;                                              \
-    HEAP_NEXT(elm, field) = NULL;                                              \
-    if (HEAP_CHILD(elm, field) == NULL)                                        \
-      return;                                                                  \
-    struct type *elm_child = HEAP_CHILD(elm, field);                           \
-    struct type *x = name##_HEAP_COMBINE_SIBLINGS(elm_child);                  \
-    HEAP_ROOT(head) = name##_HEAP_MELD(x, HEAP_ROOT(head));                    \
-  }                                                                            \
-  void name##_HEAP_INSERT(struct name *head, struct type *elm) {               \
-    assert(HEAP_CHILD(elm, field) == NULL && HEAP_PREV(elm, field) == NULL &&  \
-           HEAP_NEXT(elm, field) == NULL);                                     \
-    if (HEAP_ROOT(head) != NULL) {                                             \
-      HEAP_ROOT(head) = name##_HEAP_MELD(elm, HEAP_ROOT(head));                \
-    } else {                                                                   \
-      HEAP_ROOT(head) = elm;                                                   \
-    }                                                                          \
-  }\
+#define HEAP_IMPL(name, type, field, cmp)                                    \
+	static struct type *name##_HEAP_MELD(struct type *a, struct type *b) \
+	{                                                                    \
+		assert(HEAP_NEXT(a, field) == NULL);                         \
+		if ((cmp)(a, b)) {                                           \
+			if (HEAP_CHILD(a, field) != NULL) {                  \
+				HEAP_PREV(HEAP_CHILD(a, field), field) = b;  \
+			}                                                    \
+			if (HEAP_NEXT(b, field) != NULL) {                   \
+				HEAP_PREV(HEAP_NEXT(b, field), field) = a;   \
+			}                                                    \
+			HEAP_NEXT(a, field) = HEAP_NEXT(b, field);           \
+			HEAP_NEXT(b, field) = HEAP_CHILD(a, field);          \
+			HEAP_CHILD(a, field) = b;                            \
+			HEAP_PREV(b, field) = a;                             \
+			return a;                                            \
+		}                                                            \
+		if (HEAP_CHILD(b, field) != NULL) {                          \
+			HEAP_PREV(HEAP_CHILD(b, field), field) = a;          \
+		}                                                            \
+		if (HEAP_PREV(a, field) != NULL &&                           \
+		    HEAP_CHILD(HEAP_PREV(a, field), field) != a) {           \
+			HEAP_NEXT(HEAP_PREV(a, field), field) = b;           \
+		}                                                            \
+		HEAP_PREV(b, field) = HEAP_PREV(a, field);                   \
+		HEAP_PREV(a, field) = b;                                     \
+		HEAP_NEXT(a, field) = HEAP_CHILD(b, field);                  \
+		HEAP_CHILD(b, field) = a;                                    \
+		return b;                                                    \
+	}                                                                    \
+	static struct type *name##_HEAP_COMBINE_SIBLINGS(struct type *left)  \
+	{                                                                    \
+		HEAP_PREV(left, field) = NULL;                               \
+		struct type *root = NULL;                                    \
+		struct type *a = left;                                       \
+		while (1) {                                                  \
+			if (HEAP_NEXT(a, field) == NULL) {                   \
+				root = a;                                    \
+				break;                                       \
+			}                                                    \
+			struct type *b = HEAP_NEXT(a, field);                \
+			HEAP_NEXT(a, field) = NULL;                          \
+			b = name##_HEAP_MELD(a, b);                          \
+			if (HEAP_NEXT(b, field) == NULL) {                   \
+				root = b;                                    \
+				break;                                       \
+			}                                                    \
+			a = HEAP_NEXT(b, field);                             \
+		}                                                            \
+		while (1) {                                                  \
+			if (HEAP_PREV(root, field) == NULL) {                \
+				return root;                                 \
+			}                                                    \
+			struct type *b = HEAP_PREV(root, field);             \
+			HEAP_NEXT(b, field) = NULL;                          \
+			root = name##_HEAP_MELD(b, root);                    \
+		}                                                            \
+	}                                                                    \
+	struct type *name##_HEAP_DELETE_MIN(struct name *head)               \
+	{                                                                    \
+		if (HEAP_ROOT(head) == NULL)                                 \
+			return NULL;                                         \
+		struct type *root = HEAP_ROOT(head);                         \
+		if (HEAP_CHILD(root, field) != NULL) {                       \
+			struct type *root_child = HEAP_CHILD(root, field);   \
+			HEAP_ROOT(head) =                                    \
+				name##_HEAP_COMBINE_SIBLINGS(root_child);    \
+		} else {                                                     \
+			HEAP_ROOT(head) = NULL;                              \
+		}                                                            \
+		HEAP_CHILD(root, field) = NULL;                              \
+		HEAP_PREV(root, field) = NULL;                               \
+		HEAP_NEXT(root, field) = NULL;                               \
+		return root;                                                 \
+	}                                                                    \
+	void name##_HEAP_REMOVE(struct name *head, struct type *elm)         \
+	{                                                                    \
+		if (HEAP_PREV(elm, field) == NULL) {                         \
+			assert(HEAP_ROOT(head) == elm);                      \
+			name##_HEAP_DELETE_MIN(head);                        \
+			return;                                              \
+		}                                                            \
+		struct type *prev = HEAP_PREV(elm, field);                   \
+		if (HEAP_NEXT(elm, field) != NULL) {                         \
+			struct type *elm_next = HEAP_NEXT(elm, field);       \
+			HEAP_PREV(elm_next, field) = prev;                   \
+		}                                                            \
+		if (HEAP_CHILD(prev, field) == elm) {                        \
+			HEAP_CHILD(prev, field) = HEAP_NEXT(elm, field);     \
+		} else {                                                     \
+			HEAP_NEXT(prev, field) = HEAP_NEXT(elm, field);      \
+		}                                                            \
+		HEAP_PREV(elm, field) = NULL;                                \
+		HEAP_NEXT(elm, field) = NULL;                                \
+		if (HEAP_CHILD(elm, field) == NULL)                          \
+			return;                                              \
+		struct type *elm_child = HEAP_CHILD(elm, field);             \
+		struct type *x = name##_HEAP_COMBINE_SIBLINGS(elm_child);    \
+		HEAP_ROOT(head) = name##_HEAP_MELD(x, HEAP_ROOT(head));      \
+	}                                                                    \
+	void name##_HEAP_INSERT(struct name *head, struct type *elm)         \
+	{                                                                    \
+		assert(HEAP_CHILD(elm, field) == NULL &&                     \
+		       HEAP_PREV(elm, field) == NULL &&                      \
+		       HEAP_NEXT(elm, field) == NULL);                       \
+		if (HEAP_ROOT(head) != NULL) {                               \
+			HEAP_ROOT(head) =                                    \
+				name##_HEAP_MELD(elm, HEAP_ROOT(head));      \
+		} else {                                                     \
+			HEAP_ROOT(head) = elm;                               \
+		}                                                            \
+	}\
--- tests.c
+++ tests.c
@@ -5,96 +5,113 @@
 #include <stdlib.h>
 
 struct timer {
-  int deadline;
-  HEAP_ENTRY(timer) entry;
+	int deadline;
+	HEAP_ENTRY(timer) entry;
 };
 
-int timer_cmp(struct timer *a, struct timer *b) {
-  return a->deadline < b->deadline;
+int timer_cmp(struct timer *a, struct timer *b)
+{
+	return a->deadline < b->deadline;
 }
 
 HEAP_HEAD(timer_heap, timer) head;
 HEAP_IMPL(timer_heap, timer, entry, timer_cmp);
 
 #define NUM_TIMERS 10 * 1000 * 1000
-void million_values() {
-  struct timer *timers = calloc(NUM_TIMERS, sizeof(struct timer));
-  assert(timers != NULL);
+void million_values()
+{
+	struct timer *timers = calloc(NUM_TIMERS, sizeof(struct timer));
+	assert(timers != NULL);
 
-  int value = 0;
-  for (int i = 0; i < NUM_TIMERS; i++) {
-    if (i % 100 == 0)
-      value += 1;
-    timers[i].deadline = value;
-  }
+	int value = 0;
+	for (int i = 0; i < NUM_TIMERS; i++) {
+		if (i % 100 == 0)
+			value += 1;
+		timers[i].deadline = value;
+	}
 
-  HEAP_INIT(&head);
+	HEAP_INIT(&head);
 
-  for (int i = 0; i < NUM_TIMERS; i++) {
-    HEAP_INSERT(timer_heap, &head, &timers[i]);
-  }
+	for (int i = 0; i < NUM_TIMERS; i++) {
+		HEAP_INSERT(timer_heap, &head, &timers[i]);
+	}
 
-  int count = 0;
-  int last = 0;
-  struct timer *elm;
+	int count = 0;
+	int last = 0;
+	struct timer *elm;
 
-  while ((elm = HEAP_POP(timer_heap, &head)) != NULL) {
-    count++;
-    assert(elm->deadline >= last);
-    last = elm->deadline;
-  }
+	while ((elm = HEAP_POP(timer_heap, &head)) != NULL) {
+		count++;
+		assert(elm->deadline >= last);
+		last = elm->deadline;
+	}
 
-  assert(HEAP_POP(timer_heap, &head) == NULL);
-  assert(count == NUM_TIMERS);
+	assert(HEAP_POP(timer_heap, &head) == NULL);
+	assert(count == NUM_TIMERS);
 
-  free(timers);
+	free(timers);
 }
 
-void simple_heap() {
-  struct timer t1 = {.deadline = 100,
-                     .entry = {.prev = NULL, .child = NULL, .next = NULL}};
+void simple_heap()
+{
+	struct timer t1 = {
+		.deadline = 100,
+		.entry = { .prev = NULL, .child = NULL, .next = NULL }
+	};
 
-  struct timer t2 = {.deadline = 150,
-                     .entry = {.prev = NULL, .child = NULL, .next = NULL}};
+	struct timer t2 = {
+		.deadline = 150,
+		.entry = { .prev = NULL, .child = NULL, .next = NULL }
+	};
 
-  struct timer t3 = {.deadline = 25,
-                     .entry = {.prev = NULL, .child = NULL, .next = NULL}};
+	struct timer t3 = {
+		.deadline = 25,
+		.entry = { .prev = NULL, .child = NULL, .next = NULL }
+	};
 
-  struct timer t4 = {.deadline = 10,
-                     .entry = {.prev = NULL, .child = NULL, .next = NULL}};
+	struct timer t4 = {
+		.deadline = 10,
+		.entry = { .prev = NULL, .child = NULL, .next = NULL }
+	};
 
-  struct timer t5 = {.deadline = 500,
-                     .entry = {.prev = NULL, .child = NULL, .next = NULL}};
+	struct timer t5 = {
+		.deadline = 500,
+		.entry = { .prev = NULL, .child = NULL, .next = NULL }
+	};
 
-  struct timer t6 = {.deadline = 50,
-                     .entry = {.prev = NULL, .child = NULL, .next = NULL}};
+	struct timer t6 = {
+		.deadline = 50,
+		.entry = { .prev = NULL, .child = NULL, .next = NULL }
+	};
 
-  struct timer t7 = {.deadline = 50,
-                     .entry = {.prev = NULL, .child = NULL, .next = NULL}};
+	struct timer t7 = {
+		.deadline = 50,
+		.entry = { .prev = NULL, .child = NULL, .next = NULL }
+	};
 
-  HEAP_INIT(&head);
+	HEAP_INIT(&head);
 
-  HEAP_INSERT(timer_heap, &head, &t1);
-  HEAP_INSERT(timer_heap, &head, &t2);
-  HEAP_INSERT(timer_heap, &head, &t3);
-  HEAP_INSERT(timer_heap, &head, &t4);
-  HEAP_INSERT(timer_heap, &head, &t5);
-  HEAP_REMOVE(timer_heap, &head, &t1);
-  HEAP_INSERT(timer_heap, &head, &t6);
-  HEAP_INSERT(timer_heap, &head, &t7);
-  assert(HEAP_POP(timer_heap, &head)->deadline == 10);
-  assert(HEAP_POP(timer_heap, &head)->deadline == 25);
-  assert(HEAP_POP(timer_heap, &head)->deadline == 50);
-  assert(HEAP_POP(timer_heap, &head)->deadline == 50);
-  assert(HEAP_POP(timer_heap, &head)->deadline == 150);
-  assert(HEAP_POP(timer_heap, &head)->deadline == 500);
+	HEAP_INSERT(timer_heap, &head, &t1);
+	HEAP_INSERT(timer_heap, &head, &t2);
+	HEAP_INSERT(timer_heap, &head, &t3);
+	HEAP_INSERT(timer_heap, &head, &t4);
+	HEAP_INSERT(timer_heap, &head, &t5);
+	HEAP_REMOVE(timer_heap, &head, &t1);
+	HEAP_INSERT(timer_heap, &head, &t6);
+	HEAP_INSERT(timer_heap, &head, &t7);
+	assert(HEAP_POP(timer_heap, &head)->deadline == 10);
+	assert(HEAP_POP(timer_heap, &head)->deadline == 25);
+	assert(HEAP_POP(timer_heap, &head)->deadline == 50);
+	assert(HEAP_POP(timer_heap, &head)->deadline == 50);
+	assert(HEAP_POP(timer_heap, &head)->deadline == 150);
+	assert(HEAP_POP(timer_heap, &head)->deadline == 500);
 }
 
-int main() {
+int main()
+{
+	simple_heap();
 
-  simple_heap();
+	million_values();
 
-  million_values();
-
-  return 0;
+	return 0;
 }
